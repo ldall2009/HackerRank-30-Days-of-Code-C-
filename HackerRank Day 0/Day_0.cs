5e15e64cad6918f8cee425f0163fd5351e4c804a
Lance Dall <ldall2009@gmail.com>
2019-12-15 14:32:04 -0800

    Implemented Day 0

---
+++ HackerRank Day 0/Day_0.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace HackerRank_Day_0
+{
+    class Day_0
+    {
+        static void Main(string[] args)
+        {
+            // EXTRA: Print a string literal asking the user to enter a string to be displayed
+            Console.Write("Enter a string to be displayed: ");
+
+            // Declare a variable named 'inputString' to hold our input.
+            String inputString;
+
+            // Read a full line of input from stdin (cin) and save it to our variable, input_string.
+            inputString = Console.ReadLine();
+
+            // Line of code that prints the contents of input_string to stdout.
+            Console.WriteLine("The string you have entered is: " + inputString);
+        }
+    }
+}
