faad99b83ac90e143c3f0ce19f84d97bb01817b2
Lance Dall <ldall2009@gmail.com>
2019-12-20 09:42:01 -0800

    Implemented Day 5

---
+++ HackerRank Day 5/README.md
@@ -0,0 +1,24 @@
+# HackerRank Day 5
+
+### Objective
+
+In this challenge, we're going to use loops to help us do some simple math.
+
+### Task
+
+Given an integer, n, print its first 10 multiples. 
+Each multiple n * i (where i <= i <= 10) should be printed on a new 
+line in the form: `n x i = result`.
+
+### Input Format
+
+A single integer, n.
+
+### Constraints
+
+2 <= n <= 20
+
+### Output Format
+
+Print 10 lines of output; each line i (where 1 <= 1 <= 10) contains 
+the result of n * i in the form: `n x i = result`.
---
+++ HackerRank Day 5/day_5.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace HackerRank_Day_5
+{
+    class day_5
+    {
+        static void Main(string[] args) {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++){
+                Console.WriteLine(n + " x " + i + " = " + (n*i));
+            }
+        }
+    }
+}
