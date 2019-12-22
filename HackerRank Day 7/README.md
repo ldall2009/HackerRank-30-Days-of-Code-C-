329d47f3638037655a7cb92d19294de6b23a216a
Lance Dall <ldall2009@gmail.com>
2019-12-22 10:01:50 -0800

    Implemented Day 7

---
+++ HackerRank Day 7/README.md
@@ -0,0 +1,26 @@
+# HackerRank Day 7
+
+### Objective
+
+Today, we're learning about the Array data structure.
+
+### Task
+
+Given an array, A, of N integers, print A's elements in reverse order as 
+a single line of space-separated numbers.
+
+### Input Format
+
+The first line contains an integer, N (the size of our array).
+The second line contains N space-separated integers describing array A's 
+elements.
+
+### Constraints
+
+- 1 <= N <= 1000
+- 1 <= A_i <= 10000, where A_i is the ith integer in the array.
+
+### Output Format
+
+Print the elements of array A in reverse order as a single line of space-
+separated numbers.
---
+++ HackerRank Day 7/day_7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace HackerRank_Day_7
+{
+    class day_7
+    {
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => 
+	        Convert.ToInt32(arrTemp));
+
+            for (int i = arr.Length - 1; i >= 0; i--){
+            	Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
