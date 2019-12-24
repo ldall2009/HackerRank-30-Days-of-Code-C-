1e0fc4a92424828f12e7d17803be06e3df40fa83
Lance Dall <ldall2009@gmail.com>
2019-12-24 10:42:36 -0800

    Implemented Day 9

---
+++ HackerRank Day 9/README.md
@@ -0,0 +1,25 @@
+# HackerRank Day 9
+
+### Objective
+
+Today, we're learning and practicing an algorithmic concept called Recursion.
+
+[image here]
+
+### Task
+
+Write a factorial function that takes a positive integer, N 
+as a parameter and prints the result of N! (N factorial).
+
+### Input Format
+
+A single integer, N (the argument to pass to factorial).
+
+### Constraints
+
+- 2 <= N <= 12
+- Your submission must contain a recursive function named factorial.
+
+### Output Format
+
+Print a single integer denoting N! .
---
+++ HackerRank Day 9/day_9.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace HackerRank_Day_9
+{
+    class day_9
+    {
+        static int factorial(int n) {
+            if (n <= 1){
+                return 1;
+            }else {
+                return n * factorial(n - 1);
+            }
+        }
+
+        static void Main(string[] args) {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = factorial(n);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
