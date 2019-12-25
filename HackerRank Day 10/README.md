6ba80c5c6fa7e8ac435f1315d27f68f238caacc1
Lance Dall <ldall2009@gmail.com>
2019-12-25 11:42:26 -0800

    Implemented Day 10

---
+++ HackerRank Day 10/README.md
@@ -0,0 +1,24 @@
+# HackerRank Day 10
+
+### Objective
+
+Today, we're working with binary numbers.
+
+### Task
+
+Given a base-10 integer, n, convert it to binary (base-2). 
+Then find and print the base-10 integer denoting the maximum 
+number of consecutive 1's in n's binary representation.
+
+### Input Format
+
+A single integer, n.
+
+### Constraints
+
+- 1 <= n <= 10^6
+
+### Output Format
+
+Print a single base-10 integer denoting the maximum number of consecutive 
+1's in the binary representation of n.
---
+++ HackerRank Day 10/day_10.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace HackerRank_Day_10
+{
+    class day_10
+    {
+        static void Main(string[] args) {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            int consecutive = 0;
+            List<int> list = new List<int>();
+        
+            while (n > 0){
+                list.Add(n % 2);
+                n = n / 2;
+            }
+
+            for (int i = list.Count - 1; i >= 0; i--){
+                if (list[i] == 1){
+                    count++;
+                } else {
+                    count = 0;
+                } if (count > consecutive) {
+                    consecutive = count;
+                }
+            }
+            Console.WriteLine(consecutive);
+        }
+    }
+}
