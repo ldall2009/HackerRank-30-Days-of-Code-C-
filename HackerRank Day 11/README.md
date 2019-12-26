fdb0847ea71d7d1e0be2ec95499f299c071154c5
Lance Dall <ldall2009@gmail.com>
2019-12-26 10:19:59 -0800

    Implemented Day 11

---
+++ HackerRank Day 11/README.md
@@ -0,0 +1,39 @@
+# HackerRank Day 11
+
+### Objective
+
+Today, we're building on our knowledge of Arrays by adding another dimension.
+
+### Context
+
+Given a 6x6 2D array, A:
+
+[image]
+
+We define an hourglass in A to be a subset of values with indices falling
+in this pattern in A's graphical representation:
+
+[image]
+
+There are 16 hourglasses in A, and an hourglass sum is the sum of an
+hourglass' values
+
+### Task
+
+Calculate the hourglass sum for every hourglass in A, then print the
+maximum hourglass sum.
+
+### Input Format
+
+There are 6 lines of input, where each line contains 6 space-separated
+integers describing 2D Array A; every value in A will be in the inclusive
+range of -9 to 9.
+
+### Constraints
+
+- -9 <= A[i][j] <= 9
+- 0 <= i,j <= 5
+
+### Output Format
+
+Print the largest(maximum) hourglass sum found in A.
---
+++ HackerRank Day 11/day_11.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace HackerRank_Day_11
+{
+    class day_11
+    {
+        static int getLargestHourglassSum(int[][] arr)
+        {
+
+            int max = Int32.MinValue;
+
+            for (int i = 0; i < arr.GetLength(0) - 2; i++){
+
+                /** can not use arr.GetLength(1) for the 2nd for loop exit condition,
+                as internal arrays can have different sizes */
+
+                for (int j = 0; j < arr[i].Length - 2; j++){
+
+                    //get the addition of all cells that make the hourglass
+                    int sum = (arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + 
+                        arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] 
+                        + arr[i + 2][j + 2]);
+                        
+                    if(max < sum){
+                        max = sum;
+                    }
+                }
+            }
+
+        return max;
+        }
+
+        static void Main(string[] args) {
+            int[][] arr = new int[6][];
+
+            for (int i = 0; i < 6; i++) {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            Console.WriteLine(getLargestHourglassSum(arr));
+        }
+    }
+}
