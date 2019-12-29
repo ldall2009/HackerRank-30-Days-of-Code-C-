840d92d3c28cb644f43f97204fba830aca2412a5
Lance Dall <ldall2009@gmail.com>
2019-12-29 09:46:13 -0800

    Implemented Day 14

---
+++ HackerRank Day 14/day_14.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace HackerRank_Day_14
+{
+
+    class Difference {
+        private int[] elements;
+        public int maximumDifference;
+
+        public Difference(int[] elements){
+            this.elements = elements;
+        }
+
+        public void computeDifference(){
+            int max = Int32.MinValue;
+            int min = Int32.MaxValue;
+
+            for (int i = 0; i < elements.Length; i++) {
+                if (elements[i] > max){
+                    max = elements[i];
+                } if (elements[i] < min) {
+                    min = elements[i];
+                }
+            }
+            maximumDifference = Math.Abs(max - min);
+        }
+    } 
+
+    class Solution {
+        static void Main(string[] args) {
+            Convert.ToInt32(Console.ReadLine());
+        
+            int[] a = Console.ReadLine().Split(' ').Select(x=>Convert.ToInt32(x)).ToArray();
+        
+            Difference d = new Difference(a);
+        
+            d.computeDifference();
+        
+            Console.Write(d.maximumDifference);
+        }
+    }
+
+}
