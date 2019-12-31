6a43480e4ca91fc62d92ef39ec08d17f61b6dc95
Lance Dall <ldall2009@gmail.com>
2019-12-31 09:44:37 -0800

    Implemented Day 16

---
+++ HackerRank Day 16/README.md
@@ -0,0 +1,23 @@
+# HackerRank Day 16
+
+### Objective
+
+Today, we're getting started with Exceptions by learning how to parse an 
+integer from a string and print a custom error message.
+
+### Task
+
+Read a string, S, and print its integer value; if S cannot be converted to 
+an integer, print `Bad String`.
+
+Note: You must use the String-to-Integer and exception handling 
+constructs built into your submission language.
+
+### Input Format
+
+A single string, S.
+
+### Output Format
+
+Print the parsed integer value of S, or `Bad String` if S cannot be converted
+ to an integer.
---
+++ HackerRank Day 16/day_16.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace HackerRank_Day_16
+{
+    class day_16
+    {
+        static void Main(String[] args) {
+            string S = Console.ReadLine();
+
+            try {
+                Console.WriteLine(Int32.Parse(S));
+
+            } catch (FormatException e) {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }    
+}
