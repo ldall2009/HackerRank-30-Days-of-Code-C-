5e7654cd271ae60f4bdc2827aaaa017facff6a0e
Lance Dall <ldall2009@gmail.com>
2020-01-09 11:41:57 -0800

    Implemented Day 25

---
+++ HackerRank Day 25/README.md
@@ -0,0 +1,27 @@
+# HackerRank Day 25
+
+### Objective
+
+Today we're learning about running time!
+
+### Task
+
+A prime is a natural number greater than 1 that has no positive divisors other 
+than 1 and itself. Given a number, n, determine and print whether it's Prime 
+or Not prime.
+
+Note: If possible, try to come up with a O(sqrt(n)) primality algorithm.
+
+### Input Format
+
+The first line contains an integer, T, the number of test cases.
+Each of the T subsequent lines contains an integer, n, to be tested for primality.
+
+### Constraints
+
+- 1 <= T <= 30
+- 1 <= n <= 2 * 10^9
+
+### Output Format
+
+For each test case, print whether n is `Prime` or `Not prime` on a new line.
---
+++ HackerRank Day 25/day_25.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace HackerRank_Day_25
+{
+    class Solution {
+
+        public static String isPrime(int n) {
+
+            if (n < 2){
+                return "Not prime";
+            }
+            int sqrt = (int) Math.Sqrt(n);
+
+            /** if any whole number above 1 and below or up to sqrt(n) 
+            *  divides evenly into n, then n cannot be a prime number.
+            */
+            for (int i = 2; i <= sqrt; i++){
+                if (n % i == 0){
+                    return "Not prime";
+                }
+
+            }
+
+            return "Prime";
+        }
+
+        static void Main(String[] args) {
+        
+            int testCaseNum = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[testCaseNum];
+
+            for (int i = 0; i < array.Length; i++){
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            for (int i = 0; i < array.Length; i++){
+                Console.WriteLine(isPrime(array[i]));
+            }
+        }
+    }
+}
