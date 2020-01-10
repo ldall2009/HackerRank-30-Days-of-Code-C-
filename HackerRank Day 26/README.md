5336093316eaf99c7cb4049d1569b5ad316661ce
Lance Dall <ldall2009@gmail.com>
2020-01-10 11:12:18 -0800

    Implemented Day 26

---
+++ HackerRank Day 26/README.md
@@ -0,0 +1,41 @@
+# HackerRank Day 26
+
+### Objective
+
+Today's challenge puts your understanding of nested 
+conditional statements to the test.
+
+### Task
+
+Your local library needs your help! Given the expected and actual return 
+dates for a library book, create a program that calculates the fine 
+(if any). The fee structure is as follows:
+
+- If the book is returned on or before the expected return date, no fine 
+will be charged (i.e.: fine = 0).
+- If the book is returned after the expected return day but still within the 
+same calendar month and year as the expected return date, 
+fine = 15 * (the number of days late).
+- If the book is returned after the expected return month but still within the 
+same calendar year as the expected return date, the 
+fine = 500 * (the number of months late).
+- If the book is returned after the calendar year in which it was expected, 
+there is a fixed fine of 10000.
+
+### Input Format
+
+The first line contains 3 space-separated integers denoting the respective day, 
+month, and year on which the book was actually returned.
+The second line contains 3 space-separated integers denoting the respective day, 
+month, and year on which the book was expected to be returned (due date).
+
+### Constraints
+
+- 1 <= D <= 31
+- 1 <= M <= 12
+- 1 <= Y <= 3000
+- It is guaranteed that the dates will be valid Gregorian calendar dates.
+
+### Output Format
+
+Print a single integer denoting the library fine for the book received as input.
---
+++ HackerRank Day 26/day_26.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.CodeDom.Compiler;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+
+namespace HackerRank_Day_26
+{
+    class Solution {
+
+        static int getLateFee(int[] returned, int[] expected){
+
+            const int YEAR_LATE_FEE = 10000;
+
+            // if it is turned in a calendar year before expected, no fee
+            if (returned [2] < expected [2]){
+                return 0;
+            }
+
+            // if the year is different, there is a fixed fine of 10000
+            if (returned[2] > expected[2]){
+                return YEAR_LATE_FEE;
+            }
+
+            // if the month is different, its 500 * num of months late
+            if (returned[1] > expected[1]){
+                return 500 * (returned[1] - expected[1]);
+            
+            }
+
+            // if the day is different, its 15 * num of days late
+            if (returned[0] > expected[0]){
+                return 15 * (returned[0] - expected[0]);
+            }
+        
+            return 0;
+        }
+
+        static void Main(String[] args) {
+
+            const int ARRAY_SIZE = 3;
+
+            int[] returned = new int[ARRAY_SIZE];
+            int[] expected = new int[ARRAY_SIZE];
+
+            // get returned date values
+            string input = Console.ReadLine();
+            string[] values = input.Split(' ');
+            returned = Array.ConvertAll(values, int.Parse); // fails if the format is invalid
+
+            // get expected date values
+            input = Console.ReadLine();
+            values = input.Split(' ');
+            expected = Array.ConvertAll(values, int.Parse);
+
+            Console.WriteLine(getLateFee(returned, expected));
+
+        }
+    }
+}
