b8822551a03d426c8fa30390a8c99f5799f82ba0
Lance Dall <ldall2009@gmail.com>
2020-01-12 10:59:48 -0800

    Implemented Day 28

---
+++ HackerRank Day 28/day_28.cs
@@ -0,0 +1,42 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace HackerRank_Day_28
+{
+    class Solution {
+
+        static void Main(string[] args) {
+
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            List<string> names = new List<string>();
+            string emailSignature = "@gmail.com";
+            Regex expression = new Regex(emailSignature);
+
+            for (int NItr = 0; NItr < N; NItr++) {
+
+                string[] firstNameEmailID = Console.ReadLine().Split(' ');
+                string firstName = firstNameEmailID[0];
+                string emailID = firstNameEmailID[1];
+
+                if (expression.IsMatch(emailID)){
+                    names.Add(firstName);
+                }
+            }
+
+            names.Sort();
+            names.ForEach(Console.WriteLine);
+        }
+    }
+}
