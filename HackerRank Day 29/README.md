61fc8ed2dee1169cddb0851e6ef40eda215591d7
Lance Dall <ldall2009@gmail.com>
2020-01-13 10:53:59 -0800

    Implemented Day 29

---
+++ HackerRank Day 29/README.md
@@ -0,0 +1,27 @@
+# HackerRank Day 29
+
+### Objective
+
+Welcome to the last day! Today, we're discussing bitwise operations.
+
+### Task
+
+Given set `S = {1, 2, 3, ..., N}`, find two integers, A and B (Where A < B), from 
+set S such that the values of `A & B` is the maximum possible and also less than 
+a given integer, K.  In this case, & represents the bitwise AND operator.
+
+### Input Format
+
+The first line contains an integer, T, the number of test cases.
+Each of the T subsequent lines defines a test case as 2 space-separated integers, 
+N and K, respectively.
+
+### Constraints
+
+- 1 <= T <= 10^3
+- 2 <= N <= 10^3
+- 2 <= K <= N
+
+### Output Format
+
+For each test case, print the maximum possible value of `A & B` on a new line.
---
+++ HackerRank Day 29/day_29.cs
@@ -0,0 +1,56 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace HackerRank_Day_29
+{
+    class Solution {
+
+        static int ComputeMax(int n, int k){
+
+            int[] arr = new int[n];
+            int max = 0;
+            int andVal = 0;
+
+            // initialize array content
+            for (int i = 0; i < arr.Length; i++){
+                arr[i] = i + 1;
+            }
+
+            for (int i = 0; i < arr.Length - 1; i++){
+                for (int j = i + 1; j < arr.Length; j++){
+                    andVal = arr[i] & arr[j];
+                    if (andVal < k && andVal > max){
+                        max = andVal;
+                    }
+                }
+            }
+
+            return max;
+        }
+
+        static void Main(string[] args) {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++) {
+                string[] nk = Console.ReadLine().Split(' ');
+                int n = Convert.ToInt32(nk[0]);
+
+                int k = Convert.ToInt32(nk[1]);
+
+                Console.WriteLine(ComputeMax(n, k));
+            }
+       
+        }
+    }
+}
